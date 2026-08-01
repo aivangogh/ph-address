6f57793ba38acdb485e1f057e844a6462a2b065d
Ivan Gemota <55120258+aivangogh@users.noreply.github.com>
2026-08-01 22:27:57 +0800

    test: lock region display order

--- tests/region.test.ts
+++ tests/region.test.ts
@@ -1,10 +1,14 @@
 import { getAllRegions, getRegionByCode } from "../src";
-import regions from "../src/data/regions.json";
 
 describe("Get Region/s test suite", () => {
-  it("should return all regions from the JSON file", () => {
-    const result = getAllRegions();
-    expect(result).toEqual(regions);
+  it("should return regions in administrative display order", () => {
+    expect(getAllRegions().map(({ psgcCode }) => psgcCode)).toEqual([
+      "0100000000", "0200000000", "0300000000", "0400000000",
+      "1700000000", "0500000000", "0600000000", "0700000000",
+      "0800000000", "0900000000", "1000000000", "1100000000",
+      "1200000000", "1600000000", "1300000000", "1400000000",
+      "1900000000", "1800000000",
+    ]);
   });
 
   it("should find a region by code", () => {
